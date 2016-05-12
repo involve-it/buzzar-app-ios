91409c3d2106aa0ac59289c4c78a954a1e6b952d
yvdorofeev <yvdorofeev@gmail.com>
2016-05-11 20:34:55 -0700

    'create post' button added to tab bar

---
+++ Shiners/MainViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Shiners
+//
+//  Created by Yury Dorofeev on 4/23/16.
+//  Copyright © 2016 Involve IT, Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController, UITabBarControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tabBarController(tabBarController: UITabBarController, shouldSelectViewController viewController: UIViewController) -> Bool {
+        if viewController.title == "addPostPlaceholder" {
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let controller = storyboard.instantiateViewControllerWithIdentifier("addPost");
+            self.presentViewController(controller, animated: true, completion: nil)
+            return false;
+        }
+        return true;
+    }
+
+
+}
+
