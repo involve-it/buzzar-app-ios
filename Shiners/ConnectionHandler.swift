e6a32edc3e6769d71a75291e120ca99042cd1ae9
yvdorofeev <yvdorofeev@gmail.com>
2016-07-17 22:12:27 -0700

    url

--- Shiners/ConnectionHandler.swift
+++ Shiners/ConnectionHandler.swift
@@ -11,12 +11,12 @@ import SwiftDDP
 
 public class ConnectionHandler{
     //private let baseUrl = "http://192.168.1.61:3000"
-    private let baseUrl = "http://msg.webhop.org"
-    //private let baseUrl = "https://www.shiners.mobi"
+    //private let baseUrl = "http://msg.webhop.org"
+    private let baseUrl = "https://www.shiners.mobi"
     
-    private let url:String = "ws://msg.webhop.org/websocket"
+    //private let url:String = "ws://msg.webhop.org/websocket"
     //private let url:String = "ws://192.168.1.61:3000/websocket"
-    //private let url:String = "wss://www.shiners.mobi/websocket"
+    private let url:String = "wss://www.shiners.mobi/websocket"
     
     public private(set) var status: ConnectionStatus = .NotInitialized
     
