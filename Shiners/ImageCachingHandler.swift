5c7882ff8905f121dd870c01b21dc1c146125741
yvdorofeev <yvdorofeev@gmail.com>
2016-11-10 19:04:17 -0800

    increased max count of cached images to 50

--- Shiners/ImageCachingHandler.swift
+++ Shiners/ImageCachingHandler.swift
@@ -20,7 +20,7 @@ public class ImageCachingHandler{
     //private var imagesPendingUpload: Dictionary<NSURL, LocalImageEntity> = [:]
     private var failedUrls: [String] = []
     private let lockQueue = dispatch_queue_create("org.buzzar.app.Shiners.imageCachingHandler", nil);
-    private static let MAX_COUNT = 30;
+    private static let MAX_COUNT = 50;
     public static let defaultPhoto = UIImage(named: "no-image-placeholder.jpg");
     public static let defaultAccountImage = UIImage(named: "show_offliners.png");
     
