6f7d96434a01ce5a2872538247b2ca2029136c73
yvdorofeev <yvdorofeev@gmail.com>
2016-11-11 14:24:23 -0800

    messages disappearing bug fix

--- Shiners/DialogViewController.swift
+++ Shiners/DialogViewController.swift
@@ -82,7 +82,7 @@ public class DialogViewController : JSQMessagesViewController{
     func messagesPageReceived(notification:NSNotification){
         if let pendingMessagesAsyncId = self.pendingMessagesAsyncId where pendingMessagesAsyncId == notification.object as! String,
             let messages = MessagesHandler.Instance.getMessagesByRequestId(pendingMessagesAsyncId){
-            
+            self.chat.messagesRequested = true
             self.dataFromCache = false
             self.updateMessages(messages)
             if messages.contains({!($0.seen ?? true)}) || !(self.chat.seen ?? true){
@@ -92,7 +92,6 @@ public class DialogViewController : JSQMessagesViewController{
     }
     
     private func notifyUnseen(){
-        NotificationManager.sendNotification(.MyChatsUpdated, object: chat)
         let unseen = self.chat.messages.filter({!($0.seen ?? false) && $0.id != nil && $0.toUserId == AccountHandler.Instance.userId}).map({$0.id!})
         if unseen.count > 0 && UIApplication.sharedApplication().applicationState == .Active {
             ConnectionHandler.Instance.messages.messagesSetSeen(unseen, callback: { (success, errorId, errorMessage, result) in
@@ -104,6 +103,7 @@ public class DialogViewController : JSQMessagesViewController{
                     ThreadHelper.runOnBackgroundThread({ 
                         AccountHandler.Instance.saveMyChats()
                     })
+                    NotificationManager.sendNotification(.MyChatsUpdated, object: self.chat)
                 } else {
                     print("Error marking messages page seen: " + (errorMessage ?? "(null)"))
                 }
