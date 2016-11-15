9882535269e204c72f967e8d54a635f4b69554ae
yvdorofeev <yvdorofeev@gmail.com>
2016-11-14 18:35:15 -0800

    localization for photo upload

--- Shiners/UploadTakingLongView.swift
+++ Shiners/UploadTakingLongView.swift
@@ -17,8 +17,15 @@ class UploadTakingLongView: UIView{
     @IBOutlet weak var btnRetry: UIButton!
     @IBOutlet weak var btnCancel: UIButton!
     @IBOutlet weak var lblMessage: UILabel!
+    @IBOutlet weak var lblBottomMessage: UILabel!
     
     func setupSubviews(){
+        self.lblMessage.text = NSLocalizedString("Upload seems to be taking long time", comment: "Message, Upload seems to be taking long time")
+        self.lblBottomMessage.text = NSLocalizedString("We are still trying...", comment: "Message, We are still trying")
+        self.btnUploadLowerQuality.setTitle(NSLocalizedString("Try lower quality", comment: "Button, Try lower quality"), forState: .Normal)
+        self.btnRetry.setTitle(NSLocalizedString("Retry", comment: "Button, Retry"), forState: .Normal)
+        self.btnCancel.setTitle(NSLocalizedString("Cancel", comment: "Button, Cancel"), forState: .Normal)
+        
         self.btnRetry.layer.cornerRadius = 4
         self.btnCancel.layer.cornerRadius = 4
         self.btnUploadLowerQuality.layer.cornerRadius = 4
