2b03f879bd65284b6b7512f356746ee421191f1e
yvdorofeev <yvdorofeev@gmail.com>
2016-12-23 20:03:57 -0800

    ar

---
+++ Shiners/AugmentedRealityViewController.swift
@@ -0,0 +1,39 @@
+//
+//  AugmentedRealityViewController.swift
+//  Shiners
+//
+//  Created by Yury Dorofeev on 12/23/16.
+//  Copyright © 2016 Involve IT, Inc. All rights reserved.
+//
+
+import UIKit
+
+class AugmentedRealityViewController: UIViewController {
+
+    var posts = [Post]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.posts = AccountHandler.Instance.myPosts!
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Shiners/PlaceOfInterest.swift
@@ -0,0 +1,13 @@
+//
+//  PlaceOfInterest.swift
+//  Shiners
+//
+//  Created by Yury Dorofeev on 12/23/16.
+//  Copyright © 2016 Involve IT, Inc. All rights reserved.
+//
+
+import Foundation
+
+class PlaceOfInterest {
+    
+}
